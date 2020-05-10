99c7934e8c85ed2faf9c7c5eceecb7d22b72bcbb
satidthan <satidthan@gmail.com>
2020-05-09 21:30:23 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
